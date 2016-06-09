b933de8301e5bf9c2896ae1a96d218733590e977
AbhiNabera <abhinandannabera@yahoo.in>
2016-06-09 16:59:14 +0530

    Final

--- app/src/main/java/com/example/abhinabera/task0/MainActivity.java
+++ app/src/main/java/com/example/abhinabera/task0/MainActivity.java
@@ -1,48 +1,69 @@
 package com.example.abhinabera.task0;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Declaring variables.
+
     TextView count;
-    int counter;
     Button increment, reset;
     String countvalue;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Assigning the respective ids to components.
-        count = (TextView)findViewById(R.id.textView1);
-        increment = (Button)findViewById(R.id.button1);
-        reset = (Button)findViewById(R.id.button2);
+        SharedPreferences sp = getSharedPreferences("File", Context.MODE_PRIVATE);
+        counter = sp.getInt("count", 0);
+        count = (TextView) findViewById(R.id.textView1);
+        increment = (Button) findViewById(R.id.button1);
+        reset = (Button) findViewById(R.id.button2);
+        count.setText("" + counter);
 
-        //On click function for button 1.
         increment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                countvalue = count.getText().toString(); //Getting the text from text view, converting it to string and storing it in a variable.
-                counter = Integer.parseInt(countvalue); //Converting the string value to integer.
-                counter = counter + 1; //Incrementing by 1.
-                count.setText("" + counter); //Setting text in text view.
+                countvalue = count.getText().toString();
+                counter = Integer.parseInt(countvalue);
+                counter = counter + 1;
+                count.setText("" + counter);
+
 
             }
         });
 
-        //On click function for button 2.
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count.setText("" + 0); //Reset the count to 0.
+                counter = 0;
+                count.setText("" + counter);
             }
         });
 
+
+
     }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        SharedPreferences sp = getSharedPreferences("File", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = sp.edit();
+        ed.putInt("count", counter);
+        ed.apply();
+        count = (TextView) findViewById(R.id.textView1);
+        count.setText(counter + "");
+    }
+
 }
